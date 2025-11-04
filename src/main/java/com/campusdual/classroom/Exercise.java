cd5497540404c525381ce465ac66deb49c0fa67e
Marcos Ameneiros Pérez <marcosap10100@gmail.com>
2025-11-04 17:22:02 +0100

    Exercicio 19 OK

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -3,7 +3,6 @@ package com.campusdual.classroom;
 public class Exercise {
 
     private static String getTridimensionalString(int[][][] intArrayTri, int[][] flatMatrix) {
-
         int rows = intArrayTri[0].length;
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < rows; i++) {
@@ -12,19 +11,17 @@ public class Exercise {
             sb.append(stringTriMatrixRow(intArrayTri, i));
             sb.append("\n");
         }
-        sb.delete(sb.length()-1, sb.length());
+        sb.delete(sb.length() - 1, sb.length());
         return sb.toString();
-
     }
 
     private static String stringTriMatrixRow(int[][][] intArrayTri, int row) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < intArrayTri.length-1; i++){
+        for (int i = 0; i < intArrayTri.length - 1; i++) {
             sb.append(getUnidimensionalString(intArrayTri[i][row]));
             sb.append("   ");
         }
-        sb.append(getUnidimensionalString(intArrayTri[intArrayTri.length-1][row]));
-
+        sb.append(getUnidimensionalString(intArrayTri[intArrayTri.length - 1][row]));
         return sb.toString();
     }
 
@@ -37,35 +34,81 @@ public class Exercise {
         return getTridimensionalString(intArrayTri, flatMatrix);
     }
 
-    //TODO
+    // aplana [depth][rows][cols] → [rows][cols] sumando misma posición
     public static int[][] flatTridimensionalArray(int[][][] intArrayTri) {
+        int depth = intArrayTri.length;
+        int rows = intArrayTri[0].length;
+        int cols = intArrayTri[0][0].length;
 
+        int[][] result = new int[rows][cols];
+
+        for (int d = 0; d < depth; d++) {
+            for (int r = 0; r < rows; r++) {
+                for (int c = 0; c < cols; c++) {
+                    result[r][c] += intArrayTri[d][r][c];
+                }
+            }
+        }
+        return result;
     }
 
-
-    // TODO
+    // convierte una matriz en líneas separadas por \n
     public static String getBidimensionalString(int[][] intArrayBi) {
-
+        StringBuilder sb = new StringBuilder();
+        for (int r = 0; r < intArrayBi.length; r++) {
+            sb.append(getUnidimensionalString(intArrayBi[r]));
+            if (r < intArrayBi.length - 1) {
+                sb.append("\n");
+            }
+        }
+        return sb.toString();
     }
 
-    // TODO
+    // convierte un array en "1 2 3"
     public static String getUnidimensionalString(int[] uniArray) {
-
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < uniArray.length; i++) {
+            sb.append(uniArray[i]);
+            if (i < uniArray.length - 1) {
+                sb.append(" ");
+            }
+        }
+        return sb.toString();
     }
 
-    // TODO
+    // crea [columns] y lo rellena 1..columns
     public static int[] createAndPopulateUnidimensionalArray(int columns) {
-
+        int[] arr = new int[columns];
+        for (int i = 0; i < columns; i++) {
+            arr[i] = i + 1;
+        }
+        return arr;
     }
 
-    // TODO
+    // crea [rows][columns] y lo rellena en secuencia
     public static int[][] createAndPopulateBidimensionalArray(int rows, int columns) {
-
+        int[][] matrix = new int[rows][columns];
+        int value = 1;
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < columns; c++) {
+                matrix[r][c] = value++;
+            }
+        }
+        return matrix;
     }
 
-    // TODO
+    // crea [depth][rows][columns] y lo rellena en secuencia
     public static int[][][] createAndPopulateTridimensionalArray(int depth, int rows, int columns) {
-
+        int[][][] cube = new int[depth][rows][columns];
+        int value = 1;
+        for (int d = 0; d < depth; d++) {
+            for (int r = 0; r < rows; r++) {
+                for (int c = 0; c < columns; c++) {
+                    cube[d][r][c] = value++;
+                }
+            }
+        }
+        return cube;
     }
 
     public static void main(String[] args) {
